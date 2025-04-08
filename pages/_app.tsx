18b78c5a1eff60b81068d5fc3861cd5eae197bbd
alisyos <alisyos2@gmail.com>
2025-04-08 15:43:36 +0900

    키워드 분석 상세 페이지에서 네비게이션 바 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
 import Navbar from '../components/Navbar';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const isKeywordAnalysisPage = router.pathname === '/keyword-analysis';
+
   return (
     <>
-      <Navbar />
-      <div className="pt-16"> {/* 네비게이션 바 높이만큼 패딩 추가 */}
+      {!isKeywordAnalysisPage && <Navbar />}
+      <div className={!isKeywordAnalysisPage ? "pt-16" : ""}> {/* 네비게이션 바가 있을 때만 패딩 추가 */}
         <Component {...pageProps} />
       </div>
     </>
